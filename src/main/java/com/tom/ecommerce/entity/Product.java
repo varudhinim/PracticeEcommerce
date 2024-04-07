68d6266df7145ad690a17c040e0eb251b4d41bb1
Thomas <thomas1782000t@gmail.com>
2024-04-07 23:14:23 +0530

    Optimimzed Product entity

--- src/main/java/com/tom/ecommerce/entity/Product.java
+++ src/main/java/com/tom/ecommerce/entity/Product.java
@@ -14,54 +14,6 @@ public class Product {
     private String prodImg;
     private String prodOwner;
 
-    public Product() {}
 
-    public long getProdId() {
-        return prodId;
-    }
-
-    public void setProdId(long prodId) {
-        this.prodId = prodId;
-    }
-
-    public String getProdName() {
-        return prodName;
-    }
-
-    public void setProdName(String prodName) {
-        this.prodName = prodName;
-    }
-
-    public double getProdPrice() {
-        return prodPrice;
-    }
-
-    public void setProdPrice(double prodPrice) {
-        this.prodPrice = prodPrice;
-    }
-
-    public String getProdDesc() {
-        return prodDesc;
-    }
-
-    public void setProdDesc(String prodDesc) {
-        this.prodDesc = prodDesc;
-    }
-
-    public String getProdImg() {
-        return prodImg;
-    }
-
-    public void setProdImg(String prodImg) {
-        this.prodImg = prodImg;
-    }
-
-    public String getProdOwner() {
-        return prodOwner;
-    }
-
-    public void setProdOwner(String prodOwner) {
-        this.prodOwner = prodOwner;
-    }
 }
 ;
